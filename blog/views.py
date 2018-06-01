6da170b9dc04a6844b7c2a3cdd7264ee727023e2
jmallon <jmmallon@umd.edu>
2018-06-01 13:19:49 -0400

    added some content from bootstrap

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,13 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 
+
+def about(request):
+    return render(request, 'about.html')
+
+def index(request):
+    return render(request, 'index.html')
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
