bff9515c932a27d2bb97ea05f35fa30c046ef68b
jmallon <jmmallon@umd.edu>
2018-06-02 18:09:50 -0400

    added homepageview

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 from . import views
+from myapp.views import HomePageView
+
+
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', HomePageView.as_view(), name='index'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^blog$', views.post_list, name='post_list'),
-    url(r'^index$', views.index, name='index'),
 ]
